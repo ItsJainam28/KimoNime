3ac0ed32baea76235b4b21e359284308e93002c2
Jainam Patel <jainampatel68@gmail.com>
2025-07-29 20:11:05 -0400

    Initial commit

---
+++ src/test/java/com/jainam/Kimonime/KimonimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jainam.Kimonime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimonimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
